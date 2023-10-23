132993a2dfb9b242f7213de420eada39547c347c
jules <judigonnet@gmail.com>
2023-10-23 20:25:55 +0200

    add description and pictures in page Dashboard

--- assets/vue/controllers/Navbar.vue
+++ assets/vue/controllers/Navbar.vue
@@ -1,18 +1,26 @@
 <template>
-  <nav class="container">
-    <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between m-2">
-      <a href="/" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
-        <img src="img/logo.png"  width="40" height="32" alt="Logo">
-      </a>
-      <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+  <header class="row align-items-center m-2"> 
+  <div class="col-lg-2 d-flex justify-content-end">
+    <img src="img/logo.png"  width="40" height="32" alt="Logo">
+  </div>
+  <div class="col-lg-8 d-flex justify-content-center">
+    <ul class="nav col-7 col-md-auto mb-2 d-flex justify-content-start mb-md-0">
         <li><a href="#compagny" class="nav-link px-2 link-dark">{{ $t('navbar.compagny') }}</a></li>
         <li><a href="#study" class="nav-link px-2 link-dark">{{ $t('navbar.study') }}</a></li>
         <li><a href="#skills" class="nav-link px-2 link-dark">{{ $t('navbar.skills') }}</a></li>
       </ul>
+  </div>
+  <div class="col-lg-2 dropdown text-center">
+    <a href="#" class="d-block link-body-emphasis text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+      <i class="bi bi-translate"></i>
+    </a>
+    <ul class="dropdown-menu text-small" style="">
+      <li><a class="dropdown-item" href="#">Français</a></li>
 
-     
-    </header>
-  </nav>
+    </ul>
+  </div>
+      
+  </header>
 </template>
 
 <script setup>
