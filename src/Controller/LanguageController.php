69adc7e5beb9e4afb0aa5d548eaeb5cb30231df4
Digonnet jules <114817002+julesdig@users.noreply.github.com>
2023-11-03 22:50:52 +0100

    Can switch language from navbar  (#4)
    * switch langue

---
+++ src/Controller/LanguageController.php
@@ -0,0 +1,23 @@
+<?php
+
+// src/Controller/LanguageController.php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class LanguageController
+{
+
+    #[Route('/change-locale/{locale}', name: 'change-locale')]
+    public function changeLanguage(Request $request, $locale): JsonResponse
+    {
+        $validLocales = ['fr', 'en']; // Ajoutez d'autres langues supportées
+        if (in_array($locale, $validLocales)) {
+            $request->getSession()->set('_locale', $locale);
+        }
+        return new JsonResponse(['locale' => $request->getSession()->get('_locale')]);
+    }
+}
