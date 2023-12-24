5264cb7cd0764bed6b3fb20d93793dbbb5008ef4
jules <judigonnet@gmail.com>
2023-12-24 12:26:58 +0100

    Navbar update

--- assets/vue/controllers/Navbar.vue
+++ assets/vue/controllers/Navbar.vue
@@ -1,41 +1,57 @@
 <template>
-  <header class="row align-items-center m-2"> 
-  <div class="col-lg-2 d-flex justify-content-end">
-    <img src="/img/logo.png"  width="40" height="32" alt="Logo">
-  </div>
-  <div class="col-lg-8 d-flex justify-content-center">
-    <ul class="nav col-7 col-md-auto mb-2 d-flex justify-content-start mb-md-0">
-        <li><a href="/home/#compagny" class="nav-link px-2 link-dark">{{ $t('navbar.company') }}</a></li>
-        <li><a href="/home/#study" class="nav-link px-2 link-dark">{{ $t('navbar.study') }}</a></li>
-        <li><a href="/home/#skills" class="nav-link px-2 link-dark">{{ $t('navbar.skills') }}</a></li>
-      </ul>
-  </div>
-  <div class="col-lg-2 dropdown text-center">
-    <a href="#" class="d-block link-body-emphasis text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-      <i class="bi bi-translate"></i>
-    </a>
-    <ul class="dropdown-menu text-small" style="">
-      <li><a class="dropdown-item" @click="switchLocale('fr')">Français</a></li>
-      <li><a class="dropdown-item" @click="switchLocale('en')">English</a></li>
-    </ul>
-  </div>
-      
-  </header>
+  <v-app class="custom-app">
+      <v-app-bar rounded>
+        <v-img
+            :src="logoSrc"
+        ></v-img>
+        <v-spacer></v-spacer>
+        <v-row>
+          <v-col v-for="n in 3" :key="n" cols="4">
+            <v-list lines="one">
+              <v-list-item>
+                <v-btn  :href="'/home/#' + ['company', 'study', 'skills'][n-1]">{{ $t('navbar.' + ['company', 'study', 'skills'][n-1]) }}</v-btn>
+              </v-list-item>
+            </v-list>
+          </v-col>
+        </v-row>
+        <v-spacer></v-spacer>
+        <v-col cols="auto">
+          <v-btn block color="orange-darken-2" variant="flat" @click="switchLocale()" icon="mdi-translate"  ></v-btn>
+        </v-col>
+        <v-col cols="auto">
+          <v-btn @click="toggleTheme" icon="mdi-theme-light-dark"></v-btn>
+        </v-col>
+      </v-app-bar>
+  </v-app>
 </template>
 
-<script >
-import axios from 'axios';
+<script setup>
+import { useTheme } from 'vuetify'
+import { ref, watchEffect } from 'vue'
+const theme = useTheme()
+const logoSrc = ref('/img/logo-blanc.png')
 
+watchEffect(() => {
+  logoSrc.value = theme.global.current.value.dark ? '/img/logo-blanc.png' : '/img/logo-noir.png'
+})
+function toggleTheme () {
+  theme.global.name.value = theme.global.current.value.dark ? 'light' : 'dark'
+}
+
+</script>
+<script>
 export default {
   methods: {
-    switchLocale(locale) {
-      this.$i18n.locale = locale;
+    switchLocale() {
+      this.$i18n.locale = this.$i18n.locale === 'fr' ? 'en' : 'fr';
     }
   },
 };
 </script>
-<style lang="scss">
-@import './../../styles/navbar.scss';
-
-
+<style scoped>
+.custom-app {
+  height: 64px;
+  max-height: 64px;
+}
 </style>
+
